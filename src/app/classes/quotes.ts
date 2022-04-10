752355a1aae3076ff20e9fdfe455f1c4a3db2db0
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-10 18:15:13 +0300

    generate a new quote using the constructor in the quotes class

--- src/app/classes/quotes.ts
+++ src/app/classes/quotes.ts
@@ -1,4 +1,4 @@
-export class Quotes {
+export class Quote {
     constructor(public quote:string, public quoteAuthor:string, public submittedBy:string, public dateSubmitted:string, public upvotes:number, public downvotes:number ) {
     }
 }
--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/app/classes/quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes: Quote [] = [
+    new Quote ("What you do makes a difference. But you have to decide what kind of difference you want to make", "Aubrey Graham", "Edith", "02/04/2022", 0, 0),
+   new Quote ("Success is not final; Failure is not fatal; it is the courage to continue that counts ", "Robert Mugabe", "Lou", "02/01/2021", 0, 0),
+   new Quote ("One day or Day One: You decide", "Yoweri Museveni", "Becky", "18/04/2021", 0, 0),
+   new Quote ("Coding is 5% typing and 95% solving problems", "Aubrey Graham", "Edith", "02/04/2022", 0, 0)
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
