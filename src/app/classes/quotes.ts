5963c50fee74dcc98827945bec25d30fcf195117
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-10 18:02:36 +0300

    create a constructor use the quotes class to act as a blueprint for generating new quotes

--- src/app/classes/quotes.ts
+++ src/app/classes/quotes.ts
@@ -1,2 +1,4 @@
 export class Quotes {
+    constructor(public quote:string, public quoteAuthor:string, public submittedBy:string, public dateSubmitted:string, public upvotes:number, public downvotes:number ) {
+    }
 }
