8e3513ca8060c6facb9697f755f6465c57d74697
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-10 21:27:33 +0300

    add output property binding from the quotes description component to the quotes component

--- src/app/classes/quotes.ts
+++ src/app/classes/quotes.ts
@@ -1,4 +1,6 @@
 export class Quote {
+    showQuoteDetails: boolean;
     constructor(public quote:string, public quoteAuthor:string, public submittedBy:string, public dateSubmitted:string, public upvotes:number, public downvotes:number ) {
+        this.showQuoteDetails = false;
     }
 }
