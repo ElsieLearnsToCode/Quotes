1ed9797f6ace20bb5f678f1f8ddc6ce33f1ab9cc
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-11 04:22:06 +0300

    add quote submission form

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -10,8 +10,6 @@ export class QuotesComponent implements OnInit {
   quotes: Quote [] = [
     new Quote ("What you do makes a difference. But you have to decide what kind of difference you want to make.", "Aubrey Graham", "Edith", "02/04/2022", 0, 0),
    new Quote ("Success is not final; Failure is not fatal; it is the courage to continue that counts.", "Robert Mugabe", "Lou", "02/01/2021", 0, 0),
-   new Quote ("After climbing a great hill, one only finds that there are many more hills to climb.", "Nelson Mandela", "Becky", "18/04/2021", 0, 0),
-   new Quote ("As we let our own light shine, we unconsciously give other people permission to do the same.", "Nelson Mandela", "Edith", "02/04/2022", 0, 0)
   ]
   
   quoteDelete(deleteQuote: boolean, index: number) {
